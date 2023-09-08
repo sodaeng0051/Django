436a89c2e0104abf53cba53441d01a73963b78f2
sodaeng <sung0051@naver.com>
2023-09-08 10:58:10 +0900

    commit

--- letter/urls.py
+++ letter/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-  path('', views.letter_log, name='letter_main2'),
+  path('', views.letter_main2, name='letter_main2'),
   path('write/', views.letter_write, name='letter_write'),
-]
+  path('<int:pk>/', views.letter_list, name='letter_list'),
+  ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- letter/views.py
+++ letter/views.py
@@ -3,7 +3,7 @@ from .models import Letter
 from .forms import LetterWrite
 
 # Create your views here.
-def letter_log(request):
+def letter_main2(request):
     letters = Letter.objects.filter()
     return render(request, 'letter/letter_main2.html', {'letters':letters})
 
@@ -11,25 +11,13 @@ def letter_write(request):
     if request.method == "POST":
         form = LetterWrite(request.POST)
         if form.is_valid():
-            todo = form.save(commit=False)
-            todo.save()
+            letter = form.save(commit=False)
+            letter.save()
             return redirect('letter_write')
     else:
         form = LetterWrite()
     return render(request, 'letter/letter_write.html', {'form': form})
 
-
-def main(request):
-    if request.method == "GET":
-        return render(request, 'letter/letter_main2.html')
-
-    elif request.method == "POST":
-        username = request.POST['userName']
-        password = request.POST['userPassword']
-        user = authenticate(request, username=userName, password=userPassword)
-
-        if user is not None:
-                login(request, user)
-                return HttpResponseRedirect(reverse('posts:write'))
-        else:
-            return render(request, 'letter/letter_main2.html')
+def letter_list(request, pk):
+    letter = Letter.objects.get(id=pk)
+    return render(request, 'letter/letter_list.html', {'letter' : letter})
